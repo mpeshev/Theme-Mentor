eed996d0cc2efbf1b9dc349098cd7462fa731010
toscho <info@toscho.de>
2013-02-16 23:16:29 +0100

    Add a file_exists() check to avoid a warning. Fixes #2

--- theme-mentor.php
+++ theme-mentor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Theme Mentor
- * Description: Theme Mentor is a cousing of the Theme-Check plugin getting deeper into the code analysis. 
+ * Description: Theme Mentor is a cousing of the Theme-Check plugin getting deeper into the code analysis.
  * It's using different approaches to monitor for common problems regarding theme reviews from the
  * WordPress Theme Reviewers Team. It is prone to fault analysis, so use only as a reference for improving
  * your code base even further.
@@ -10,7 +10,7 @@
  * Author: nofearinc
  * Author URI: http://devwp.eu/
  * License: GPLv2 or later
- * 
+ *
  */
 /*
  * Copyright (C) 2013 Mario Peshev
@@ -19,16 +19,16 @@
 	modify it under the terms of the GNU General Public License
 	as published by the Free Software Foundation; either version 2
 	of the License, or (at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * */
 
 define( 'TM_PLUGIN_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
@@ -41,29 +41,29 @@ define( 'TM_INC_URL', trailingslashit( TM_PLUGIN_URL . 'inc' ) );
 
 /**
  * The main class for the plugin, initializing everything needed and including all tests
- * 
+ *
  * @author nofearinc
  *
  */
 class Theme_Mentor {
-	
+
 	private $templates = array();
 	private $includes = array();
 	private $theme_path = '';
 	public static $validations = array();
-	
+
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'theme_mentor_page' ) );
 		add_action( 'dx_theme_mentor_before_tests_list', array( $this, 'display_theme_name_tested' ) );
 		// TODO: temporary
 // 		$this->do_everything();
 	}
-	
+
 	public function run_tests( ) {
 		// all the heavy lifting for picking up proper files from the theme folder
 		// for templates and includes, that is
 		$this->iterate_theme_folder( $this->theme_path, 0 );
-		
+
 		// swap functions.php as it's include-alike
 		$functions_file = $this->theme_path . 'functions.php';
 		foreach( $this->templates as $index => $template ) {
@@ -71,60 +71,63 @@ class Theme_Mentor {
 				unset( $this->templates[ $index ] );
 		}
 		$this->includes[] = $this->theme_path . 'functions.php';
-		
+
 		// Include check files
 		include TM_INC_PATH . 'general-theme-validations.php';
 		$general_validations = new General_Theme_Validations();
-		
+
 		// Include complex checks
 		include TM_PLUGIN_PATH . 'theme-mentor-executor.php';
 		$dir = 'inc/complex';
 		foreach (glob(dirname(__FILE__). "/{$dir}/*.php") as $file) {
 			include $file;
 		}
-		
+
 		// iterate all templates
 		foreach( $this->templates as $index => $template ) {
 			// only unique theme stuff
 			$template_unique_only = str_replace( $this->theme_path , '',  $template );
-				
+
 			// read the files, keep the file number as it matters, you know
 			$file = file( $template , FILE_IGNORE_NEW_LINES );
 			if( false === $file ) { continue; }
-				
+
 			// General
 			foreach( $general_validations->common_validations as $pattern => $message ) {
 				$this->iterate_data( $pattern, $message, $template_unique_only, $file );
 			}
-			
+
 			foreach( $general_validations->template_validations as $pattern => $message ) {
 				$this->iterate_data( $pattern, $message, $template_unique_only, $file );
 			}
-			
+
 			foreach( self::$validations as $validation ) {
 				$validation->crawl( $template, $file );
 			}
 		}
-		
+
 		// iterate includes
 		foreach( $this->includes as $index => $functional ) {
 			// only unique theme stuff
 			$functional_unique_only = str_replace( $this->theme_path , '',  $functional );
-		
+
+			if ( ! file_exists( $functional ) ) {
+				continue;
+			}
 			// read the files, keep the file number as it matters, you know
 			$file = file( $functional , FILE_IGNORE_NEW_LINES );
 			if( false === $file ) { continue; }
-		
+
 			// General
 			foreach( $general_validations->common_validations as $pattern => $message ) {
 				$this->iterate_data( $pattern, $message, $functional_unique_only, $file );
 			}
-				
+
 			foreach( $general_validations->include_validations as $pattern => $message ) {
 				$this->iterate_data( $pattern, $message, $functional_unique_only, $file );
 			}
 		}
-		
+
 		// display complex validations errors
 		foreach( self::$validations as $validation ) {
 			$validation->execute();
@@ -135,7 +138,7 @@ class Theme_Mentor {
 			}
 		}
 	}
-	
+
 	/**
 	 * Adapt the Theme Mentor page
 	 */
@@ -143,7 +146,7 @@ class Theme_Mentor {
 		$page = add_theme_page( 'Theme Mentor', 'Theme Mentor', 'manage_options', 'theme_mentor', array( $this, 'theme_mentor_page_cb' ) );
 		add_action( 'admin_print_styles-' . $page, array( $this, 'styles_theme_mentor' ) );
 	}
-	
+
 	/**
 	 * Admin page callback
 	 */
@@ -151,7 +154,7 @@ class Theme_Mentor {
 		if( ! current_user_can( 'manage_options' ) ) {
 			wp_die( __( 'We all know you shouldn\'t be here', 'dx_theme_mentor' ) );
 		}
-		
+
 		// get stylesheet to pick the selected theme
 		$stylesheet = get_stylesheet();
 		// default activated theme is selected atfirst
@@ -162,9 +165,9 @@ class Theme_Mentor {
 		echo '<div class="wrap">';
 		echo '<div id="icon-edit" class="icon32 icon32-base-template"><br></div>';
 		echo '<h2>' . __( 'Theme Mentor', 'dx_theme_mentor' ) . '</h2>';
-		
+
 		do_action( 'dx_theme_mentor_before_tests_list' );
-		
+
 		// is the form submitted
 		if( isset( $_POST['dx_theme'] ) ) {
 			$theme_name = $_POST['dx_theme'];
@@ -172,23 +175,23 @@ class Theme_Mentor {
 			if( isset( $themes[$theme_name] ) ) {
 				$theme = $themes[$theme_name];
 				$this->theme_path = trailingslashit( $theme->get_template_directory() );
-				
+
 				// selected is the last submitted to $_POST
 				$selected = $theme->get_stylesheet();
-				
+
 				$this->run_tests();
 			}
 		}
-		
+
 		do_action( 'dx_theme_mentor_before_admin_page' );
 
 		include_once 'inc/templates/admin-template.php';
-		
+
 		do_action( 'dx_theme_mentor_after_admin_page' );
-		
+
 		echo '</div>';
 	}
-	
+
 	/**
 	 * Iterate theme folder and assign templates and includes
 	 * @param string $folder folder path
@@ -198,11 +201,11 @@ class Theme_Mentor {
 		// get all templates
 		$folder = trailingslashit( $folder );
 		$directory = dir( $folder );
-		
+
 		while ( false !== ( $entry = $directory->read() ) ) {
 			// drop all empty folders, hidden folders/files and parents
 			if ( ( $entry[0] == "." ) ) continue;
-			
+
 			// includes should be there
 			if( is_dir( $folder . $entry ) ) {
 				// iterate the next level
@@ -221,10 +224,10 @@ class Theme_Mentor {
 			}
 		}
 	}
-	
+
 	/**
-	 * Do the regex for the possibly dangerous snippets 
-	 * 
+	 * Do the regex for the possibly dangerous snippets
+	 *
 	 * @param regex $pattern
 	 * @param error message text $message
 	 * @param path to file when something happened $file_path
@@ -234,28 +237,28 @@ class Theme_Mentor {
 		$lines_found = preg_grep( $pattern, $file );
 		if( ! empty( $lines_found ) ) {
 			foreach( $lines_found as $line => $snippet ) {
-				printf( '<div class="tm_report_row"><span class="tm_message">%s</span> at file <span class="tm_file">%s</span>, line <span class="tm_line">%d</span>: <span class="tm_snippet">%s</span></div>', 
+				printf( '<div class="tm_report_row"><span class="tm_message">%s</span> at file <span class="tm_file">%s</span>, line <span class="tm_line">%d</span>: <span class="tm_snippet">%s</span></div>',
 						$message, $file_path, $line + 1, esc_html( $snippet ) );
 			}
 		}
 	}
-	
+
 	// list all errors
-	
+
 	// lookout for the errors
 	// admin panel for theme list
-	
+
 	/**
 	 * Enqueue styles for admin
 	 */
 	public function styles_theme_mentor() {
 		wp_enqueue_style( 'theme-mentor', TM_PLUGIN_URL . 'css/theme-mentor.css' );
 	}
-	
+
 	public function display_theme_name_tested( ) {
 		if( ! empty( $_POST['dx_theme'] ) ) {
 			$testing_word = __('Testing', 'dx_theme_mentor');
-			printf('<h2>%s %s...</h2>', $testing_word, $_POST['dx_theme']); 
+			printf('<h2>%s %s...</h2>', $testing_word, $_POST['dx_theme']);
 		}
 	}
 }
